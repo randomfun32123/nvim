0ab13992248c85ce4dadf0b63eeb7315b82c7dc7
NBB <nbb@randomfun.me>
2026-03-24 01:05:36 -0400

    fix deprecation

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -63,18 +63,19 @@ return {
 
             vim.diagnostic.config({
                 virtual_text = true,
-                signs = true,
                 underline = true,
                 update_in_insert = false,
                 severity_sort = true,
                 float = { border = "rounded" },
+                signs = {
+                    text = {
+                        [vim.diagnostic.severity.ERROR] = " ",
+                        [vim.diagnostic.severity.WARN]  = " ",
+                        [vim.diagnostic.severity.HINT]  = "󰠠 ",
+                        [vim.diagnostic.severity.INFO]  = " ",
+                    },
+                },
             })
-
-            local signs = { Error = " ", Warn = " ", Hint = "󰠠 ", Info = " " }
-            for type, icon in pairs(signs) do
-                local hl = "DiagnosticSign" .. type
-                vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
-            end
 
             local handlers = {
                 ["textDocument/hover"] = vim.lsp.with(
